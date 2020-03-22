78987da18fcd1b4827dbac685134298b00693e08
ChanYeolL <403427693@qq.com>
2020-03-22 22:53:58 +0800

    Titanic: 数据分析1

---
+++ Titanic/dataAnalysis.py
@@ -0,0 +1,11 @@
+#-----------------------------
+#
+#
+#-----------------------------
+
+import pandas as pd #数据分析
+import numpy as np #科学计算
+from pandas import Series,DataFrame
+
+data_train = pd.read_csv("Titanic/data/train.csv")
+print(data_train)
