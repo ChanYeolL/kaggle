9cedf283c7e7688ed134448251b6fb93f7696f31
ChanYeolL <403427693@qq.com>
2020-03-29 08:39:11 +0800

    Titanic: 数据分析

--- Titanic/dataAnalysis.py
+++ Titanic/dataAnalysis.py
@@ -1,11 +1,12 @@
-#-----------------------------
-#
-#
-#-----------------------------
+#-----------------------------------------------------
+# Editor:Chanyeol Liu    Date:2020/03/28
+# Code:CL200328
+# Purpose:任务二：基于深度学习的文本分类
+#-----------------------------------------------------
 
 import pandas as pd #数据分析
 import numpy as np #科学计算
 from pandas import Series,DataFrame
 
-data_train = pd.read_csv("Titanic/data/train.csv")
-print(data_train)
+data_train = pd.read_csv("data/train.csv")
+print(data_train.describe())
